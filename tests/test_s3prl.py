7aa8e6fc82f65a591466a47ebc3f5b342fd04f83
mxmpl <44207491+mxmpl@users.noreply.github.com>
2026-07-22 15:36:24 +0200

    increase tolerance in CPU S3PRL tests
    due to SDPA implementation

--- tests/test_s3prl.py
+++ tests/test_s3prl.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import NotRequired, TypedDict
 
 import pytest
 import torch
@@ -7,9 +8,19 @@ from s3prl.upstream.hubert.convert import load_converted_model
 from s3prl.upstream.hubert.hubert_model import HubertConfig, HubertModel, HubertPretrainingConfig
 
 from minimal_hubert import HuBERT, HuBERTPretrain
+
 from .conftest import hypothesis_settings, waveforms
 
 S3PRL_CKPT_URL = "https://huggingface.co/s3prl/converted_ckpts/resolve/main/hubert_base_ls960.pt"
+
+
+class Tolerance(TypedDict):
+    rtol: NotRequired[float]
+    atol: NotRequired[float]
+
+
+def tolerance(device: torch.device) -> Tolerance:
+    return {"rtol": 1e-4, "atol": 1e-4} if device.type == "cpu" else {}
 
 
 @pytest.fixture(scope="module")
@@ -43,7 +54,7 @@ def test_s3prl_encoder_intermediate(
     y = my_hubert.get_intermediate_outputs(waveforms, before_residual=False)
     for layer, yi in enumerate(y):
         xi = s3prl_hubert.extract_features(waveforms, output_layer=layer + 1)[0]
-        torch.testing.assert_close(xi, yi)
+        torch.testing.assert_close(xi, yi, **tolerance(device))
 
 
 @pytest.fixture(scope="module")
@@ -60,7 +71,7 @@ def s3prl_pretrained_hubert(s3prl_ckpt_path: Path, device: torch.device) -> Hube
 
 
 @pytest.fixture(scope="module")
-def my_pretrained_hubert(s3prl_pretrained_hubert: HubertModel, s3prl_ckpt_path: Path, device: torch.device) -> HuBERT:
+def my_pretrained_hubert(s3prl_ckpt_path: Path, device: torch.device) -> HuBERT:
     return HuBERT.from_pretrained(s3prl_ckpt_path).eval().to(device)
 
 
@@ -77,4 +88,4 @@ def test_s3prl_pretrained_encoder_intermediate(
     y = my_pretrained_hubert.get_intermediate_outputs(waveforms, before_residual=False)
     for layer, yi in enumerate(y):
         xi = s3prl_pretrained_hubert.extract_features(waveforms, output_layer=layer + 1)[0]
-        torch.testing.assert_close(xi, yi)
+        torch.testing.assert_close(xi, yi, **tolerance(device))
